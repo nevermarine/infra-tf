71d7437eecb1c7aacb9927ac6c0b104c068fe7b9
Maxim Fedotov <mvfedotov_1@edu.hse.ru>
2024-05-20 12:01:37 +0300

    Change almalinux URL

--- images.tf
+++ images.tf
@@ -1,5 +1,5 @@
 module "almalinux9" {
   source    = "./modules/image"
-  image_url       = "https://repo.almalinux.org/almalinux/9/cloud/x86_64/images/AlmaLinux-9-GenericCloud-9.3-20231113.x86_64.qcow2"
+  image_url       = "https://mirror.yandex.ru/almalinux/9/cloud/x86_64/images/AlmaLinux-9-GenericCloud-latest.x86_64.qcow2"
   image_file_name = "almalinux9.3.qcow2.img"
 }

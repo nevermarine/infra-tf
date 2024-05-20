97a76e1ad9321cccef85b71b2d6c79ed0a210c75
Maxim Fedotov <mvfedotov_1@edu.hse.ru>
2024-05-20 12:19:49 +0300

    Move sensitive vars to tfvars and add auth.tf

---
+++ auth.tf
@@ -0,0 +1,19 @@
+variable "username" {
+  type = string
+}
+
+variable "password" {
+  type = string
+}
+
+variable "ssh_key" {
+  type = string
+}
+
+variable "proxmox_endpoint" {
+  type = string
+}
+
+variable "ssh_username" {
+  type = string
+}

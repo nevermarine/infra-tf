59b2e041d0780cad34488f1eac2c69446e70d00d
Maxim Fedotov <mvfedotov_1@edu.hse.ru>
2024-06-09 23:54:51 +0300

    style: Remove comment

--- vms.tf
+++ vms.tf
@@ -44,7 +44,6 @@ module "NAS" {
     file_id      = module.almalinux9.image_id
     datastore_id = "local-lvm"
   }
-  # passthrough_disk = ["/dev/sda", "/dev/sdb"]
   passthrough_disk = [
     {
       path = "/dev/sda"
